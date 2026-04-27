60d8e808d55e64a1e203b38c08a9083ff6dea824
Nail Ayyasy <ayyasyx90@gmail.com>
2026-04-27 19:12:19 +0700

    Pertemuan 8 week 9

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,56 @@
 <?php
-use App\Http\Controllers\DosenController;
+
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DosenController ;
+use App\Http\Controllers\PegawaiController ;
+use App\Http\Controllers\BlogController ;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('halo', function () {
-    return "Halo, Selamat datang di tutorial Laravel www.malasngoding.com";
+	return "<h1>Halo, Selamat datang</h1>di tutorial laravel <i>www.malasngoding.com</i>";
 });
 
-Route::get('blog', function () {
-    return view('blog');
+
+Route::get('biodata', function () {
+	return view('biodata');
 });
 
-Route::get('pert5', function () {
-    return view ('pertemuan5');
+Route::get('p1', function () {
+	return view('intro');
+});
+
+Route::get('p2', function () {
+	return view('news');
+});
+
+
+Route::get('p3', function () {
+	return view('responsive');
+});
+
+Route::get('p4', function () {
+	return view('5026241101');
+});
+
+Route::get('p5', function () {
+	return view('pertemuan5');
+});
+
+Route::get('/', function () {
+	return view('welcome');
 });
 
 Route::get('dosen', [DosenController::class, 'index']);
+Route::get('biodata', [DosenController::class, 'biodata']);
+
+Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+Route::get('/formulir', [PegawaiController::class, 'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+
+// route blog
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
